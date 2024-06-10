217fa8a789d0a77eff78a187263f671a775e62fa
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-10 11:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.onload=function() {
+	let browserName=navigator.userAgent;
+	let appname=navigator.appName;
+	let appVersion=navigator.appVersion;
+	let divelement=document.getElementById("browser-info");
+	divelement.innerHTML=`<p>You are using ${browserName} version ${appVersion}</p>`
+}
